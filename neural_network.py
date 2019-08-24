64c8f22d0e981e1f442fc4e153584010cc1d9761
galah92 <galaharoni92@gmail.com>
2019-08-24 22:23:21 +0300

    extract_features() fixup

--- neural_network.py
+++ neural_network.py
@@ -20,12 +20,11 @@ class Network(nn.Module):
 
 def neural_network_model(trees, samples, vocab, tag_to_ind_map, num_iters=200):
     num_classes = len(ind_toaction_map)
-    x_train, _ = extract_features(trees, samples, vocab, 1, tag_to_ind_map)
+    x_train, y_train = extract_features(trees, samples, vocab, None, tag_to_ind_map)
     net = Network(len(x_train[0]), hidden_size=128, num_classes=num_classes)
     print(net)
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(net.parameters(), lr=1e-4, momentum=0.9)
-    x_train, y_train = extract_features(trees, samples, vocab, None, tag_to_ind_map)
     for i in range(num_iters):
         y_pred = net(torch.autograd.Variable(torch.tensor(x_train)))
         var = torch.autograd.Variable(torch.tensor(y_train))
