566c0f119884c8542cabc698becd74e9e3d9f30e
galah92 <galaharoni92@gmail.com>
2019-08-24 16:33:33 +0300

    glove files are now saved inside git dir

--- glove.py
+++ glove.py
@@ -1,26 +1,26 @@
 from pathlib import Path
-import tempfile
 import urllib.request
-import numpy as np
 import zipfile
+import numpy as np
 
 
 GLOVE_URL = 'http://nlp.stanford.edu/data/glove.6B.zip'
-GLOVE_PATH = Path(tempfile.gettempdir())
-GLOVE_ZIP = GLOVE_PATH / 'glove.6B.zip'
-GLOVE_6B_50D = GLOVE_PATH / 'glove.6B.50d.txt'
+GLOVE_DIR = Path(__file__).resolve().parent / 'glove'
+GLOVE_ZIP = GLOVE_DIR / 'glove.6B.zip'
+GLOVE_6B_50D = GLOVE_DIR / 'glove.6B.50d.txt'
 
 
 def get_word_vectors(tokens):
 
     if not GLOVE_ZIP.is_file():
         print(f'downloading glove files to {GLOVE_ZIP}')
+        GLOVE_DIR.mkdir(exist_ok=True)
         urllib.request.urlretrieve(GLOVE_URL, str(GLOVE_ZIP))
 
     if not GLOVE_6B_50D.is_file():
         with zipfile.ZipFile(GLOVE_ZIP, 'r') as f:
             print(f'extracting {GLOVE_ZIP}')
-            f.extractall(str(GLOVE_PATH))
+            f.extractall(str(GLOVE_DIR))
 
     word_vectors = np.zeros((len(tokens), 50))
     with GLOVE_6B_50D.open(encoding='utf8') as f:
